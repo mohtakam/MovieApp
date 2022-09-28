31c1c68f0b6363b59b74ff94a35d90804e0cfa33
mohtakam <101166466+mohtakam@users.noreply.github.com>
2022-09-28 23:52:24 +0530

    Add files via upload

---
+++ src/common/commonStore.js
@@ -0,0 +1,19 @@
+import { createStore } from "redux";
+
+const initialState = {
+  filterMovies: [],
+};
+
+function handleStores(state = initialState, action) {
+  switch (action.type) {
+    case "FILTER_MOVIES":
+      //return {...state,"subscribers":action.payload}
+      console.log(action.payload);
+
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default createStore(handleStores);
